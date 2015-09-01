03c42edad1a7a54a56662b651641f1d671d90dcf
KostadinP <kostadin.popkocev@gmail.com>
2015-09-01 16:16:24 +0200

    FetchDataService for getting the line

--- app/src/main/java/com/example/transportmk/transportmk/FetchDataService.java
+++ app/src/main/java/com/example/transportmk/transportmk/FetchDataService.java
@@ -1,20 +1,29 @@
 package com.example.transportmk.transportmk;
 
+import android.annotation.TargetApi;
 import android.app.IntentService;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.util.Log;
 
+import com.example.transportmk.transportmk.model.Line;
+import com.example.transportmk.transportmk.model.Schedule;
 import com.example.transportmk.transportmk.model.Station;
+import com.example.transportmk.transportmk.model.Station$Table;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.raizlabs.android.dbflow.sql.builder.Condition;
+import com.raizlabs.android.dbflow.sql.language.Select;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 /**
  * An {@link IntentService} subclass for handling asynchronous task requests in
@@ -23,10 +32,12 @@ import java.net.URL;
  * helper methods.
  */
 public class FetchDataService extends IntentService {
-    // TODO: Rename actions, choose action names that describe tasks that this
-    // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
+
     private static final String ACTION_FETCH_STATIONS = "com.example.transportmk.transportmk.action.FOO";
-    private static final String ACTION_BAZ = "com.example.transportmk.transportmk.action.BAZ";
+    private static final String ACTION_FETCH_LINE = "com.example.transportmk.transportmk.action.BAZ";
+
+    private static final String EXTRA_FROMCITY = "com.example.transportmk.transportmk.extra.PARAM1";
+    private static final String EXTRA_TOCITY = "com.example.transportmk.transportmk.extra.PARAM2";
 
     public final String LOG_TAG = this.getClass().getSimpleName();
 
@@ -48,10 +59,11 @@ public class FetchDataService extends IntentService {
      *
      * @see IntentService
      */
-    // TODO: Customize helper method
-    public static void startActionBaz(Context context, String param1, String param2) {
+    public static void startActionFetchLine(Context context, String param1, String param2) {
         Intent intent = new Intent(context, FetchDataService.class);
-        intent.setAction(ACTION_BAZ);
+        intent.setAction(ACTION_FETCH_LINE);
+        intent.putExtra(EXTRA_FROMCITY, param1);
+        intent.putExtra(EXTRA_TOCITY, param2);
         context.startService(intent);
     }
 
@@ -65,8 +77,10 @@ public class FetchDataService extends IntentService {
             final String action = intent.getAction();
             if (ACTION_FETCH_STATIONS.equals(action)) {
                 handleActionFoo();
-            } else if (ACTION_BAZ.equals(action)) {
-                handleActionBaz();
+            } else if (ACTION_FETCH_LINE.equals(action)) {
+                final String param1 = intent.getStringExtra(EXTRA_FROMCITY);
+                final String param2 = intent.getStringExtra(EXTRA_TOCITY);
+                handleActionBaz(param1, param2);
             }
         }
     }
@@ -150,8 +164,89 @@ public class FetchDataService extends IntentService {
      * Handle action Baz in the provided background thread with the provided
      * parameters.
      */
-    private void handleActionBaz() {
-        // TODO: Handle action Baz
-        throw new UnsupportedOperationException("Not yet implemented");
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    private void handleActionBaz(String param1, String param2) {
+        // These two need to be declared outside the try/catch
+// so that they can be closed in the finally block.
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+
+// Will contain the raw JSON response as a string.
+        String jsonStr = null;
+
+        try {
+            // Construct the URL for the OpenWeatherMap query
+            // Possible parameters are available at OWM's forecast API page, at
+            // http://openweathermap.org/API#forecast
+            URL url = new URL("https://transport-mk.herokuapp.com/data/rest/lines/schedulesByStations");
+
+            Station from = new Select().from(Station.class).where(
+                    Condition.column(Station$Table.STATIONNAME).eq(param1)).querySingle();
+            Station to = new Select().from(Station.class).where(
+                    Condition.column(Station$Table.STATIONNAME).eq(param2)).querySingle();
+            String urlParameters = "startStationId=" + from.getId() + "&endStationId=" + to.getId();
+            byte[] postData = urlParameters.getBytes(StandardCharsets.UTF_8);
+            int postDataLength = postData.length;
+
+            // Create the request to OpenWeatherMap, and open the connection
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            urlConnection.setRequestProperty("charset", "utf-8");
+            urlConnection.setRequestProperty("Content-Length", Integer.toString(postDataLength));
+            urlConnection.setUseCaches(false);
+            try (DataOutputStream wr = new DataOutputStream(urlConnection.getOutputStream())) {
+                wr.write(postData);
+            }
+            urlConnection.connect();
+
+            // Read the input stream into a String
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null) {
+                // Nothing to do.
+                return;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                // But it does make debugging a *lot* easier if you print out the completed
+                // buffer for debugging.
+                buffer.append(line + "\n");
+            }
+
+            if (buffer.length() == 0) {
+                // Stream was empty.  No point in parsing.
+                return;
+            }
+            jsonStr = buffer.toString();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Error ", e);
+            // If the code didn't successfully get the weather data, there's no point in attempting
+            // to parse it.
+            return;
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (final IOException e) {
+                    Log.e(LOG_TAG, "Error closing stream", e);
+                }
+            }
+        }
+        parseLineJson(jsonStr);
+    }
+
+    private void parseLineJson(String jsonStr) {
+        Gson gson = new Gson();
+        Line line = gson.fromJson(jsonStr, Line.class);
+        for (Schedule sc : line.getScheduleList()) {
+            Log.v("TAG", sc.getId() + " " + sc.getDepartureTime());
+        }
     }
 }
--- app/src/main/java/com/example/transportmk/transportmk/FormFragment.java
+++ app/src/main/java/com/example/transportmk/transportmk/FormFragment.java
@@ -1,11 +1,7 @@
 package com.example.transportmk.transportmk;
 
-import android.annotation.TargetApi;
-import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,20 +9,9 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 
 import com.example.transportmk.transportmk.adapters.StationAdapter;
-import com.example.transportmk.transportmk.model.Line;
-import com.example.transportmk.transportmk.model.Schedule;
 import com.example.transportmk.transportmk.model.Station;
-import com.google.gson.Gson;
 import com.raizlabs.android.dbflow.sql.language.Select;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 
@@ -52,15 +37,6 @@ public class FormFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_form, container, false);
 
-        Button button = (Button) rootView.findViewById(R.id.btnGetLine);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.v("TAG", "do something on btn click");
-                new FetchLineTask().execute();
-            }
-        });
-
         mList = new Select().from(Station.class).queryList();
         tbFrom = (AutoCompleteTextView) rootView.findViewById(R.id.tbFrom);
         tbTo = (AutoCompleteTextView) rootView.findViewById(R.id.tbTo);
@@ -68,97 +44,15 @@ public class FormFragment extends Fragment {
         tbFrom.setAdapter(adapter);
         tbTo.setAdapter(adapter);
 
+        Button button = (Button) rootView.findViewById(R.id.btnGetLine);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FetchDataService.startActionFetchLine(getActivity(), tbFrom.getText().toString(), tbTo.getText().toString());
+            }
+        });
+
+
         return rootView;
-    }
-
-    public class FetchLineTask extends AsyncTask<Void, Void, Void> {
-
-        public final String LOG_TAG = FetchLineTask.class.getSimpleName();
-
-        @TargetApi(Build.VERSION_CODES.KITKAT)
-        protected Void doInBackground(Void... params) {
-            // These two need to be declared outside the try/catch
-// so that they can be closed in the finally block.
-            HttpURLConnection urlConnection = null;
-            BufferedReader reader = null;
-
-// Will contain the raw JSON response as a string.
-            String jsonStr = null;
-
-            try {
-                // Construct the URL for the OpenWeatherMap query
-                // Possible parameters are available at OWM's forecast API page, at
-                // http://openweathermap.org/API#forecast
-                URL url = new URL("https://transport-mk.herokuapp.com/data/rest/lines/schedulesByStations");
-
-                String urlParameters = "startStationId=2&endStationId=12";
-                byte[] postData = urlParameters.getBytes(StandardCharsets.UTF_8);
-                int postDataLength = postData.length;
-
-                // Create the request to OpenWeatherMap, and open the connection
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("POST");
-                urlConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-                urlConnection.setRequestProperty("charset", "utf-8");
-                urlConnection.setRequestProperty("Content-Length", Integer.toString(postDataLength));
-                urlConnection.setUseCaches(false);
-                try (DataOutputStream wr = new DataOutputStream(urlConnection.getOutputStream())) {
-                    wr.write(postData);
-                }
-                urlConnection.connect();
-
-                // Read the input stream into a String
-                InputStream inputStream = urlConnection.getInputStream();
-                StringBuffer buffer = new StringBuffer();
-                if (inputStream == null) {
-                    // Nothing to do.
-                    return null;
-                }
-                reader = new BufferedReader(new InputStreamReader(inputStream));
-
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-                    // But it does make debugging a *lot* easier if you print out the completed
-                    // buffer for debugging.
-                    buffer.append(line + "\n");
-                }
-
-                if (buffer.length() == 0) {
-                    // Stream was empty.  No point in parsing.
-                    return null;
-                }
-                jsonStr = buffer.toString();
-                Log.v(LOG_TAG, jsonStr);
-                parseJson(jsonStr);
-
-
-            } catch (IOException e) {
-                Log.e(LOG_TAG, "Error ", e);
-                // If the code didn't successfully get the weather data, there's no point in attempting
-                // to parse it.
-                return null;
-            } finally {
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (final IOException e) {
-                        Log.e(LOG_TAG, "Error closing stream", e);
-                    }
-                }
-            }
-            return null;
-        }
-
-        private void parseJson(String jsonStr) {
-            Gson gson = new Gson();
-            Line line = gson.fromJson(jsonStr, Line.class);
-            for (Schedule sc : line.getScheduleList()) {
-                Log.v("TAG", sc.getId() + " " + sc.getDepartureTime());
-            }
-        }
     }
 }
